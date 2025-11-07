cc29e0d41922b897d7f6ecba1fc1fa006c80e8ef
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-11-07 16:07:34 -0500

    feat: Finish convert_encoding.rb

--- convert_encoding.rb
+++ convert_encoding.rb
@@ -1,78 +1,92 @@
 #!/usr/bin/env ruby
 
-# NOT FINISHED - DO NOT USE
+require 'bundler/inline'
+gemfile do
+  source 'https://rubygems.org'
+  gem 'charlock_holmes'
+end
 
-# require 'bundler/inline'
-# gemfile do
-#   source 'https://rubygems.org'
-#   gem 'charlock_holmes'
-#   gem 'pry'
-# end
+require 'fileutils'
+require 'optparse'
+require 'charlock_holmes'
 
-# require 'fileutils'
-# require 'optparse'
-# require 'pry'
-# require 'charlock_holmes'
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: convert_encoding.rb -i path-to-input-dir "\
+    "-s file-suffix -o path-to-output-dir"
 
-# options = {}
-# OptionParser.new do |opts|
-#   opts.banner = 'Usage: convert_encoding.rb -i path-to-input-dir -s file-suffix'
+  opts.on('-i', '--input PATH', 'Path to input directory containing files') do |i|
+    options[:input] = File.expand_path(i)
+  end
 
-#   opts.on('-i', '--input PATH', 'Path to input directory containing files') do |i|
-#     options[:input] = File.expand_path(i)
-#   end
+  opts.on('-s', '--suffix STRING', 'File suffix, without dot') do |s|
+    options[:suffix] = ".#{s.delete_prefix('.')}"
+  end
 
-#   opts.on('-s', '--suffix STRING', 'File suffix, without dot') do |s|
-#     options[:suffix] = ".#{s.delete_prefix('.')}"
-#   end
+  opts.on('-o', '--output PATH', 'Path to output directory') do |o|
+    options[:output] = File.expand_path(o)
+  end
 
-#   opts.on('-h', '--help', 'Prints this help') do
-#     puts opts
-#     exit
-#   end
-# end.parse!
+  opts.on('-h', '--help', 'Prints this help') do
+    puts opts
+    exit
+  end
+end.parse!
 
-# Detector = CharlockHolmes::EncodingDetector.new
-# Converter = CharlockHolmes::Converter.new
+class MyFile
+  attr_reader :path, :outpath
+  def initialize(path, outpath)
+    @path = path
+    @outpath = outpath
+  end
 
-# class MyFile
-#   attr_reader :path
-#   def initialize(path)
-#     @path = path
-#   end
+  def detect
+    @detected_encoding ||=
+      CharlockHolmes::EncodingDetector.detect(content)
+    end
 
-#   def convert_to_utf8
-#     puts "Converting #{path}"
-#     `iconv -f #{encoding} -t UTF-8 #{path} > #{path}.conv`
-#   end
-  
-#   def content
-#     File.read(path)
-#   end
+  def convert
+    str = converted
+    return unless str
 
-#   def encoding
-#     @encoding ||= Detector.detect(content)
-#   end
+    File.open(outpath, "w") do |f|
+      f.write(str)
+    end
+  end
 
-#   def utf8?
-#     encoding == 'UTF-8'
-#   end
+  def converted
+    str = CharlockHolmes::Converter.convert(content, encoding, 'UTF-8')
+    puts "Converted #{path} from #{encoding} to UTF-8"
+    str
+  rescue ArgumentError=> err
+    if err.message == "U_FILE_ACCESS_ERROR"
+      puts "\n#{path} too short to detect/convert\n"
+    else
+      puts "\n#{err}\n"
+    end
+    nil
+  end
 
-#   private
+  def content
+    File.read(path)
+  end
 
+  def encoding
+    @encoding ||= detect[:encoding]
+  end
 
-# end
+  def encoding_ok?
+    ["UTF-8"].include?(encoding)
+  end
+end
 
-# files = Dir.children(options[:input])
-#   .select{ |name| name.downcase.end_with?(options[:suffix]) }
-#   .map{ |name| "#{options[:input]}/#{name}" }
-#   .map{ |path| MyFile.new(path) }
+Dir.children(options[:input])
+  .select{ |name| name.downcase.end_with?(options[:suffix]) }
+  .each do |filename|
+    filepath = Pathname.new(File.join(options[:input], filename))
+    outpath = File.join(options[:output], filepath.basename)
+    file = MyFile.new(filepath, outpath)
+    next if file.encoding_ok?
 
-
-# files.each do |file|
-#   binding.pry
-# end
-
-# # to_convert = files.reject{ |file| file.utf8? }
-
-# # to_convert.each{ |file| file.convert_to_utf8 }
+    file.convert
+  end
