9a1f89adf3241d4f2848f3ce44c2cda94df4dde8
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-04-01 21:03:18 -0400

    feat: Make safe_open.rb accept multiple paths

--- safe_open.rb
+++ safe_open.rb
@@ -21,25 +21,27 @@ end
 require "csv"
 require "pry"
 
-path = ARGV[0]
-fail("No file path, or path to non-CSV, given") unless path
+paths = ARGV
+fail("No file path, or path to non-CSV, given") if paths.empty?
 
-ln = File.open(path) { |f| f.readline }
-cell_formats = CSV.parse(ln, liberal_parsing: true)
-  .flatten
-  .map { |_hdr| "2" }
-  .join("/")
+paths.each do |path|
+  ln = File.open(path) { |f| f.readline }
+  cell_formats = CSV.parse(ln, liberal_parsing: true)
+    .flatten
+    .map { |_hdr| "2" }
+    .join("/")
 
-cmd_base = 'soffice --infilter="Text - txt - csv (StarCalc):'
+  cmd_base = 'soffice --infilter="Text - txt - csv (StarCalc):'
 
-# See https://help.libreoffice.org/latest/ro/text/shared/guide/csv_params.html
-# 44 = comma field separator
-# 34 = double quote text delimiter
-# 76 = UTF-8 character set
-# 1 = line number to start reading
-# 0 = default/UI language
-# true = quoted field as text
-# false = detect special numbers
-filter = "44,34, 76,1,#{cell_formats},0,true,false\""
+  # See https://help.libreoffice.org/latest/ro/text/shared/guide/csv_params.html
+  # 44 = comma field separator
+  # 34 = double quote text delimiter
+  # 76 = UTF-8 character set
+  # 1 = line number to start reading
+  # 0 = default/UI language
+  # true = quoted field as text
+  # false = detect special numbers
+  filter = "44,34, 76,1,#{cell_formats},0,true,false\""
 
-`#{cmd_base}#{filter} #{path}`
+  `#{cmd_base}#{filter} #{path}`
+end
